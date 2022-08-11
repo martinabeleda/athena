8d217a8dc20b63f724e522ce96da616b22a8e151
Martin Abeleda <martin.abeleda@gmail.com>
2022-08-11 12:27:29 -0700

    Add post endpoint for author

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 use actix_web::{
-    body::BoxBody, get, http::header::ContentType, web, HttpRequest, HttpResponse, Responder,
+    body::BoxBody, get, http::header::ContentType, post, web, HttpRequest, HttpResponse, Responder,
 };
 use serde::{Deserialize, Serialize};
 use std::sync::Mutex;
-
 // This struct represents an author
-#[derive(Serialize, Deserialize, Debug)]
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Author {
     pub id: i32,
     pub first_name: String,
@@ -33,10 +33,19 @@ pub async fn get_author() -> impl Responder {
     }
 }
 
+#[post("/author")]
+pub async fn create(author: web::Json<Author>, data: web::Data<AppState>) -> impl Responder {
+    let mut authors = data.authors.lock().unwrap();
+    authors.authors.push(author.clone());
+    println!("Stored authors: {:?}", authors);
+
+    author
+}
+
 // This struct holds all of the authors in memory
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Authors {
-    authors: Vec<Author>,
+    pub authors: Vec<Author>,
 }
 
 impl Responder for Authors {
@@ -52,32 +61,18 @@ impl Responder for Authors {
 }
 
 #[get("/authors")]
-pub async fn list_authors() -> impl Responder {
-    Authors {
-        authors: vec![
-            Author {
-                id: 0,
-                first_name: String::from("Haruki"),
-                last_name: String::from("Murakami"),
-            },
-            Author {
-                id: 1,
-                first_name: String::from("Malcolm"),
-                last_name: String::from("Gladwell"),
-            },
-            Author {
-                id: 2,
-                first_name: String::from("George"),
-                last_name: String::from("Orwell"),
-            },
-        ],
-    }
+pub async fn list_authors(data: web::Data<AppState>) -> impl Responder {
+    let authors = data.authors.lock().unwrap().clone();
+    println!("authors: {:?}", authors);
+
+    authors
 }
 
 // This struct represents state
 pub struct AppState {
     pub app_name: String,
     pub counter: Mutex<i32>,
+    pub authors: Mutex<Authors>,
 }
 
 #[get("/hello/{name}")]
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 use actix_web::{web, App, HttpServer};
 use std::sync::Mutex;
 
-use athena::{get_author, greet, list_authors, AppState};
+use athena::{create, get_author, greet, list_authors, AppState, Author, Authors};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let counter = web::Data::new(AppState {
         app_name: String::from("Some Data"),
+        authors: Mutex::new(Authors {
+            authors: vec![Author {
+                id: 0,
+                first_name: String::from("Haruki"),
+                last_name: String::from("Murakami"),
+            }],
+        }),
         counter: Mutex::new(0),
     });
 
     HttpServer::new(move || {
         App::new().app_data(counter.clone()).service(
             web::scope("/athena/v1")
+                .service(create)
                 .service(greet)
                 .service(get_author)
                 .service(list_authors),
